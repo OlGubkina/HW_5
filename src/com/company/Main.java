1f07561647724adba0fec6dec1ab46aaca8bebfa
Olga <63593441+OlGubkina@users.noreply.github.com>
2022-01-26 15:33:45 +0200

    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	//  Создать 5 конкретных объектов класса Box.
+    //  Задать конкретное состояние этих объектов (конкретные величины ширины, высоты и длины).
+
+        Box defaultBox = new Box();
+        Box smallCube = new Box(3);
+        Box bigCube = new Box(30);
+        Box littleBox = new Box(1,2,3);
+        Box longBox = new Box(20,3,4);
+
+// Вывести на экран объемы этих коробок
+        defaultBox.printBoxVolume(defaultBox); //?
+        smallCube.printBoxVolume(smallCube);
+        bigCube.printBoxVolume(bigCube);
+        littleBox.printBoxVolume(littleBox);
+        longBox.printBoxVolume(longBox);
     }
 }
